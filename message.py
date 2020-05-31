e3218254dbd4778530763c4e60878283aa89bc95
cossaq <max.k@berkeley.edu>
2020-05-30 23:29:48 -0700

    fix

--- message.py
+++ message.py
@@ -1,2 +1,2 @@
 # Change this to the message you want to send!
-MESSAGE = "Hello {}, \nBeep beep, I'm CodeLink - Max's linkedin bot. You can get me at https://github.com/cossaq/cold-link.git beep beep"
+MESSAGE = "Hello {}, \nBeep beep, I'm ColdLink - Max's linkedin bot. You can get me at https://github.com/cossaq/cold-link.git beep beep"
